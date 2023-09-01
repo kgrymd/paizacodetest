eb5f3deca383305740a1863f6251a308fa93c2a4
kgrymd <kgroymd@gmail.com>
2023-09-01 21:40:59 +0900

    add 0901

---
+++ 0901/C014/js.js
@@ -0,0 +1,26 @@
+const lines = [
+    '4 2',
+    '6 6 6',
+    '4 6 4',
+    '6 1 1',
+    '4 4 4',
+]
+
+const [n, r] = lines[0].split(' ').map(Number);
+
+const boxSizes = lines.slice(1).map(str => str.split(' ').map(Number));
+
+// console.log(arrys);
+
+const getResultArray = (arrays, r) => {
+    newArray = [];
+    arrays.forEach((array, i) => {
+
+        if (Math.min(...array) >= r * 2) {
+            newArray.push(i + 1)
+        }
+    })
+    return newArray;
+};
+
+console.log([...getResultArray(boxSizes, r)].join('\n'));
